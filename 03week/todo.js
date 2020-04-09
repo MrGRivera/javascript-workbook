f49e357132c8a4e18ea2de9b83f3abad58e2991b
MrGRivera <gabrielrivera2889@gmail.com>
2020-04-09 12:35:11 -0500

    my to do project

---
+++ 03week/todo.js
@@ -0,0 +1,18 @@
+console.log("Loaded script");
+
+
+// when add button is clicked
+// append text to bottom of list
+  //add new list item
+
+// when delete button is clicked
+// remove its parent list item
+
+// when span is clicked
+// class done should be added
+
+
+
+
+//this function adds click event to the span
+
