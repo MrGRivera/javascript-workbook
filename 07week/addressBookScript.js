068851c4790413c0c21a6976ccef6ad6d68853b5
MrGRivera <gabrielrivera2889@gmail.com>
2020-05-12 14:56:16 -0500

    added to address book project

--- 07week/addressBookScript.js
+++ 07week/addressBookScript.js
@@ -1,3 +1,39 @@
 'use script'
 
 console.log("Connected");
+
+
+
+
+window.onload = function () {
+  console.log('window loaded');
+  fetchAddressBook();
+}
+
+function fetchAddressBook() {
+  fetch('https://randomuser.me/api?results=10')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      processContacts(data.results)
+    });
+}
+
+function processContacts(contacts) {
+  contacts.forEach(function (contact) {
+    let contactsUl = document.querySelector('#contacts');
+
+    let contactsName = document.createElement('li');
+    contactsName.innerText = 'Name: ' + contact.name.first;
+    contactsUl.appendChild(contactsName)
+
+    let contactsAge = document.createElement('li');
+    contactsAge.innerText = 'Age: ' + contact.dob.age;
+    contactsUl.appendChild(contactsAge)
+
+  })
+}
+
+
